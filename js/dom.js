837ff2c8933068d4318e7372e25ab7974e7074b3
DanielL492 <144807703+DanielL492@users.noreply.github.com>
2024-01-10 09:10:18 -0500

    er

--- js/dom.js
+++ js/dom.js
@@ -1,3 +1,5 @@
 const DOMSelectors = {
-
+    container: document.getElementById("flexcontainer"),
 }
+
+export {DOMSelectors}
--- js/main.js
+++ js/main.js
@@ -1,2 +1,14 @@
 import {characters} from "./arr"
+import { DOMSelectors } from "./dom"
 
+function insert(arr) {
+    arr.forEach((data) => DOMSelectors.container.insertAdjacentHTML("beforeend",
+    `<div class="flexcontainer">
+      <div class="card">
+        <h4 class="name">${data.name}</h1>
+        <img src="${arr.img}" alt="Image of ${arr.name}" class="card-img"/> <br>
+      </div>
+    </div>`
+))}
+
+insert(characters);
