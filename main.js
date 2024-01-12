8503835bd3dd4a76ea4ab54821dac8b5e3b3e1b0
DanielL492 <144807703+DanielL492@users.noreply.github.com>
2024-01-12 09:24:09 -0500

    xc

--- main.js
+++ main.js
@@ -154,7 +154,7 @@ function insert(arr) {
     arr.forEach((arr) => DOMSelectors.container.insertAdjacentHTML("beforeend",
     `<div class="flexcontainer">
       <div class="card">
-        <h4 class="name">${arr.name}</h4>
+        <h6 class="name">${arr.name}</h6>
         <img src="${arr.img}" alt="Image of ${arr.name}" class="card-img"/> <br>
       </div>
     </div>`
